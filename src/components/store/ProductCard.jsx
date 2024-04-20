ad1343a4ef9b6af9bc3a72efa8979eac48f8c861
m0usedev <alvaro.sobrino.coding@gmail.com>
2024-04-20 18:54:33 +0200

    changes ProductCard

--- src/components/store/ProductCard.jsx
+++ src/components/store/ProductCard.jsx
@@ -1,37 +1,40 @@
-
-import GOOD_RESULT from '../../JSON/good-result.json'
+import PropTypes from 'prop-types';
 
 import BaseButton from '../button/BaseButton'
 
 import './css/Product.css'
 import './css/ProductCard.css'
 
-export default function ProductCard () {
+export default function ProductCard ({ product }) {
   return (
     <div className='ProductCard'>
       <img
         className='product-img'
-        src={GOOD_RESULT.images[0]}
-        alt={GOOD_RESULT.title}
-        title={GOOD_RESULT.title}
+        src={product.images[0]}
+        alt={product.title}
+        title={product.title}
       />
-      <h3>{GOOD_RESULT.title}</h3>
+      <h3>{product.title}</h3>
       <p>
         {
-          GOOD_RESULT.description.substring(0, 20)
+          product.description.substring(0, 20)
         }...
         <span>Read More</span>
       </p>
       <div className='product-category'>
-        {GOOD_RESULT.category.name}
+        {product.category.name}
       </div>
 
       <footer>
         <div className='product-price'>
-          Price <span>$ {GOOD_RESULT.price}</span>
+          Price <span>$ {product.price}</span>
         </div>
         <BaseButton />
       </footer>
     </div>
   )
 }
+
+ProductCard.propTypes = {
+  product: PropTypes.object
+};
