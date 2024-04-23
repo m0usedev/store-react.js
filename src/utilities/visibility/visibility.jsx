f23d2e74b87243f51458289b94cb25949227768b
m0usedev <alvaro.sobrino.coding@gmail.com>
2024-04-23 12:18:48 +0200

    add visibility

---
+++ src/utilities/visibility/visibility.jsx
@@ -0,0 +1,29 @@
+import PropTypes from 'prop-types';
+
+import { useState, createContext } from "react"
+
+export function useVisibility () {
+  const [visibility, setVisibility] = useState(false)
+  return {visibility, setVisibility}
+}
+
+export const VisibilityContext = createContext()
+import './visibility.css'
+
+export function VisibilityProvider ({ children }) {
+  const {visibility, setVisibility} = useVisibility()
+  return (
+    <VisibilityContext.Provider value={{
+        visibility, 
+        setVisibility
+      }}>
+        <div className='VisibilityProvider'>
+          {children}
+        </div>
+    </VisibilityContext.Provider>
+  )
+}
+
+VisibilityProvider.propTypes = {
+  children: PropTypes.object
+};
