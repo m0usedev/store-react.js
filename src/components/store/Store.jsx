dc6c42c88f6efc9a1b4a15a986cf02699a2ba11a
m0usedev <alvaro.sobrino.coding@gmail.com>
2024-04-23 12:26:11 +0200

    implements Store

--- src/components/store/Store.jsx
+++ src/components/store/Store.jsx
@@ -1,11 +1,27 @@
+import { VisibilityProvider } from "../../utilities/visibility/visibility"
+import { ProductsProvider } from "./hooks/useProducts"
+
 import StoreHeader from "./StoreHeader"
 import ProductList from "./ProductList"
+import StorePagination from "./StorePagination"
+import { PaginationProvider } from "./utilities/usePagination"
 
 export default function Store () {
   return (
     <div className="Store">
-      <StoreHeader />
-      <ProductList />
+      <ProductsProvider>
+        <PaginationProvider>
+
+          <StoreHeader />
+
+          <VisibilityProvider>
+            <ProductList />
+          </VisibilityProvider>
+
+          <StorePagination />
+          
+        </PaginationProvider>
+      </ProductsProvider>
     </div>
   )
 }
